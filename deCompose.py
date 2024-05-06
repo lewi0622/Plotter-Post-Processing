736e17176bbc95f844be86d674073b4b65821c89
Eric Lewiston <eric.lewiston.el@gmail.com>
2024-05-06 14:45:31 -0500

    added in separate files by color option

--- deCompose.py
+++ deCompose.py
@@ -15,7 +15,7 @@ def main(input_files=()):
         global return_val
 
         if len(input_files) == 1 and last_shown_command == build_vpypeline(True):
-            rename_replace(show_temp_file, output_filename)
+            rename_replace(show_temp_file, output_file_list[0])
             print("Same command as shown file, not re-running Vpype pipeline")
         else:
             command = build_vpypeline(False)
@@ -29,10 +29,11 @@ def main(input_files=()):
     def show_vpypeline():
         """Runs given commands on first file, but only shows the output."""
         global last_shown_command
-        command = build_vpypeline(True)
-        last_shown_command = command
-        print("Showing: \n", command)
-        subprocess.run(command)
+        last_shown_command = build_vpypeline(True)
+        print("Showing: \n", last_shown_command)
+        subprocess.run(last_shown_command)
+        if separate_files.get(): #hacky solution to the separate files portion not being included in the show command
+            last_shown_command = ""
 
 
     def build_vpypeline(show):
@@ -46,8 +47,12 @@ def main(input_files=()):
         for filename in input_file_list:
             file_parts = os.path.splitext(filename)
             show_temp_file = file_parts[0] + "_show_temp_file.svg"
-            output_filename = file_parts[0] + "_deCOMPOSE.svg"
-            output_file_list.append(output_filename)
+            output_filename = file_parts[0] + "_deCOMPOSE"
+            if separate_files.get():
+                for i in range(max_colors_per_file([filename])):
+                    output_file_list.append(output_filename + str(i) + ".svg")
+            else:
+                output_file_list.append(output_filename + ".svg")
 
         args = f'vpype eval "files_in={input_file_list}" eval "files_out={output_file_list}" '
 
@@ -59,11 +64,17 @@ def main(input_files=()):
             repeat_num = 1
         args += f" repeat {repeat_num} "
 
-        # remove color layers
+        #check for any layer removal
         remove_any = False
         for index, layer in enumerate(remove_layer_list):
             if layer.get():
                 remove_any = True
+
+        if not remove_any and not separate.get() and show: #just show case
+            args += r" read -a stroke --no-crop %files_in[_i]%  end show"
+            return args
+
+        # remove color layers
         if remove_any:
             args += r" read -a stroke --no-crop %files_in[_i]% "
             for index, layer in enumerate(remove_layer_list):
@@ -82,7 +93,7 @@ def main(input_files=()):
             if remove_any:
                 file_input = show_temp_file
 
-            args += f" read -a d -a points --no-crop  {file_input} "
+            args += f" read -a d -a points --no-crop {file_input} "
             args += f" filter --min-length {min_line_len_entry.get()}in "
             args += r" forlayer "
             if override_colors.get():
@@ -93,7 +104,20 @@ def main(input_files=()):
                 args += r' linesort '
 
             if show:
-                args += f' write "{show_temp_file}" '
+                args += f' write "{show_temp_file}" ldelete all'
+
+        if separate_files.get() and not show:
+            file_input = r'%files_in[_i]%'
+            if remove_any or separate.get():
+                file_input = show_temp_file
+            args += f" read -a stroke --no-crop {file_input} "
+            args += r' eval "k=0" '
+            args += r" forlayer write " 
+            args += r' %files_out[k]% '
+            args += r' eval "k=k+1" '
+            args += r" end end"
+
+            return args
 
         if show:
             args += f' end ldelete all read -a stroke --no-crop "{show_temp_file}" show '
@@ -163,6 +187,16 @@ def main(input_files=()):
 
     linesort = IntVar(window, value=1)
     ttk.Checkbutton(window, text="Sort after splitting into layers", variable=linesort).grid(sticky="w", row=current_row, column=1, columnspan=2)
+    current_row += 1
+
+    ttk.Separator(window, orient='horizontal').grid(sticky="we", row=current_row, column=0, columnspan=4, pady=10)
+    current_row += 1
+
+    separate_files_label = ttk.Label(window, justify=CENTER, text="Separate SVG Layers into individual files\n(doesn't work with Show)", foreground=settings.link_color, cursor="hand2")
+    separate_files_label.bind("<Button-1>", lambda e: open_url_in_browser("https://vpype.readthedocs.io/en/latest/cookbook.html#saving-each-layer-as-a-separate-file"))
+    separate_files_label.grid(row=current_row, column=0, columnspan=2)
+    separate_files = IntVar(window, value=0)
+    ttk.Checkbutton(window, text="Separate\nFiles", variable=separate_files).grid(sticky="w", row=current_row, column=2)
     current_row += 1
 
     ttk.Separator(window, orient='horizontal').grid(sticky="we", row=current_row, column=0, columnspan=4, pady=10)
