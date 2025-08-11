fd3282837ff91134de1c60d8f6571946e68c0bf3
Eric Lewiston <eric.lewiston.el@gmail.com>
2025-08-10 22:50:23 -0500

    linting

--- main_application.py
+++ main_application.py
@@ -1,7 +1,12 @@
-from tkinter import *
+from tkinter import Tk, StringVar
 from tkinter import ttk
-from utils import *
-import Occult, Paint, Process, Compose, deCompose, settings
+from utils import select_files, file_info, on_closing
+import Occult
+import Paint
+import Process
+import Compose
+import deCompose
+import settings
 
 def main():
     def verify_output_files(files, util_name):
@@ -42,24 +47,35 @@ def main():
 
     global file_info_text
     file_info_text = StringVar(main_app)
-    ttk.Label(main_app, textvariable= file_info_text).grid(pady=(10,0), row=current_row, column=0, columnspan=2)
+    ttk.Label(main_app, textvariable= file_info_text
+        ).grid(pady=(10,0), row=current_row, column=0, columnspan=2)
     #TODO add button to launch separate window for file details
     current_row += 1
 
     select_files()
     file_info_text.set(f"{len(file_info["files"])} file(s) selected")
 
-    ttk.Button(main_app, text="Occult", command=run_occult).grid(pady=(2,2), row=current_row, column=0)
-    ttk.Button(main_app, text="Process", command=run_process).grid(pady=(2,2), row=current_row, column=1)
-    ttk.Button(main_app, text="Paint", command=run_paint).grid(pady=(2,2), row=current_row, column=2)
+    ttk.Button(main_app, text="Occult", command=run_occult
+        ).grid(pady=(2,2),row=current_row, column=0)
+    ttk.Button(main_app, text="Process", command=run_process
+        ).grid(pady=(2,2), row=current_row, column=1)
+    ttk.Button(main_app, text="Paint", command=run_paint
+        ).grid(pady=(2,2), row=current_row, column=2)
     current_row += 1
-    ttk.Button(main_app, text="Compose", command=run_compose).grid(pady=(2,2), row=current_row, column=0)
-    ttk.Button(main_app, text="deCompose", command=run_deCompose).grid(pady=(2,2), row=current_row, column=1)
+    ttk.Button(main_app, text="Compose", command=run_compose
+        ).grid(pady=(2,2), row=current_row, column=0)
+    ttk.Button(main_app, text="deCompose", command=run_deCompose
+        ).grid(pady=(2,2), row=current_row, column=1)
 
     current_row += 1
 
-    ttk.Button(main_app, text="Re-Select Files", command=select_files).grid(pady=(0,10), row=current_row, column=0)
-    ttk.Label(main_app, text="By default, output files from one utility are taken as\nthe new input files. Reselect files if necessary.").grid(pady=(0,10), row=current_row, column=1, columnspan=2)
+    ttk.Button(main_app, text="Re-Select Files", command=select_files
+        ).grid(pady=(0,10), row=current_row, column=0)
+    ttk.Label(
+        main_app,
+        text="""By default, output files from one utility are taken as
+        the new input files. Reselect files if necessary."""
+        ).grid(pady=(0,10), row=current_row, column=1, columnspan=2)
     current_row += 1
 
     main_app.protocol("WM_DELETE_WINDOW", lambda arg=main_app: on_closing(arg))
