2daed0b7efa1fb3ffd90f08e0a9a378271081ce9
lewi0622 <eric.lewiston.el@gmail.com>
2025-12-05 09:21:59 -0600

    removed the occlusion file selection and menu options

--- occult.py
+++ occult.py
@@ -114,23 +114,13 @@ def main(input_files=()):
     return_val = ()
     last_shown_command = ""
 
-    occlusion_file_list = get_files("SELECT OCCLUSION FILE(s) (OPTIONAL)")
-    if len(occlusion_file_list) > 0:
-        print(f"Currently Loaded Occlusion Files: {occlusion_file_list}")
-    else:
-        print("No Occlusion files selected")
-
     svg_width_inches = file_info["size_info"][0][0]  # first file first item
     svg_height_inches = file_info["size_info"][0][1]  # first file second item
 
     color_list = []
     # generate color list for each potential -k
-    for file in input_files + occlusion_file_list:
+    for file in input_files:
         color_list.append(generate_random_color())
-
-    occult_color_list = []
-    for file in occlusion_file_list:
-        occult_color_list.append(generate_random_color())
 
     # tk widgets and window
     current_row = 0  # helper row var, inc-ed every time used;
@@ -145,14 +135,14 @@ def main(input_files=()):
     title.grid(pady=(10, 0), row=current_row, column=0, columnspan=4)
     current_row += 1
 
-    ttk.Label(window, justify=CENTER, text=f"{len(input_files)} Design file(s) selected, {len(occlusion_file_list)} Occlusion file(s) selected, \nDesign file Width(in): {svg_width_inches}, Height(in): {svg_height_inches}").grid(
+    ttk.Label(window, justify=CENTER, text=f"{len(input_files)} Design file(s) selected \nDesign file Width(in): {svg_width_inches}, Height(in): {svg_height_inches}").grid(
         row=current_row, column=0, columnspan=4)
     current_row += 1
 
     ttk.Label(window, text="Parse Design file using: ").grid(
         row=current_row, column=0)
     attribute = IntVar(window, value=0)
-    if max_colors_per_file() == 1 or len(occlusion_file_list) > 0:
+    if max_colors_per_file() == 1:
         attribute = IntVar(window, value=1)
     ttk.Radiobutton(window, text="d/point", variable=attribute,
                     value=0).grid(row=current_row, column=1)
@@ -166,100 +156,39 @@ def main(input_files=()):
     current_row += 1
 
     occult_info_list = []
+    occult_file_info = {}
+    occult_order = ttk.Combobox(
+        window,
+        values=[0]
+    )
+    occult_order.current(0)
+    occult_file_info["order"] = occult_order
 
-    if len(occlusion_file_list) == 0:
-        occult_file_info = {}
-        occult_order = ttk.Combobox(
-            window,
-            values=[0]
-        )
-        occult_order.current(0)
-        occult_file_info["order"] = occult_order
+    occult = IntVar(window, value=1)
+    occult_file_info["occult"] = occult
+    ttk.Checkbutton(window, text="Occult", variable=occult).grid(
+        sticky="w", row=current_row, column=0)
+    occult_keep_lines = IntVar(window, value=0)
+    occult_file_info["keep"] = occult_keep_lines
+    ttk.Checkbutton(window, text="Keep occulted lines", variable=occult_keep_lines).grid(
+        sticky="w", row=current_row, column=1)
+    current_row += 1
 
-        occult = IntVar(window, value=1)
-        occult_file_info["occult"] = occult
-        ttk.Checkbutton(window, text="Occult", variable=occult).grid(
-            sticky="w", row=current_row, column=0)
-        occult_keep_lines = IntVar(window, value=0)
-        occult_file_info["keep"] = occult_keep_lines
-        ttk.Checkbutton(window, text="Keep occulted lines", variable=occult_keep_lines).grid(
-            sticky="w", row=current_row, column=1)
-        current_row += 1
+    occult_ignore = IntVar(window, value=1)
+    occult_file_info["ignore"] = occult_ignore
+    ttk.Checkbutton(window, text="Ignores layers", variable=occult_ignore).grid(
+        sticky="w", row=current_row, column=0)
+    occult_across = IntVar(window, value=0)
+    occult_file_info["across"] = occult_across
+    ttk.Checkbutton(window, text="Occult across layers,\nnot within",
+                    variable=occult_across).grid(sticky="w", row=current_row, column=1)
+    current_row += 1
 
-        occult_ignore = IntVar(window, value=1)
-        occult_file_info["ignore"] = occult_ignore
-        ttk.Checkbutton(window, text="Ignores layers", variable=occult_ignore).grid(
-            sticky="w", row=current_row, column=0)
-        occult_across = IntVar(window, value=0)
-        occult_file_info["across"] = occult_across
-        ttk.Checkbutton(window, text="Occult across layers,\nnot within",
-                        variable=occult_across).grid(sticky="w", row=current_row, column=1)
-        current_row += 1
+    ttk.Separator(window, orient='horizontal').grid(
+        sticky="we", row=current_row, column=0, columnspan=4, pady=10)
+    current_row += 1
 
-        ttk.Separator(window, orient='horizontal').grid(
-            sticky="we", row=current_row, column=0, columnspan=4, pady=10)
-        current_row += 1
-
-        occult_info_list.append(occult_file_info)
-    else:
-        for index, file in enumerate(occlusion_file_list):
-            occult_file_info = {"file": file}
-
-            name = os.path.basename(os.path.normpath(file))
-            ttk.Label(window, text=f"File: {name}").grid(
-                row=current_row, column=0, columnspan=2)
-
-            ttk.Label(window, text=f"Order: ").grid(row=current_row, column=2)
-            occult_order = ttk.Combobox(
-                window,
-                width=4,
-                state="readonly",
-                values=[*range(len(occlusion_file_list))]
-            )
-            occult_order.current(index)
-            occult_order.grid(sticky="w", row=current_row, column=3)
-            occult_file_info["order"] = occult_order
-            current_row += 1
-
-            occult_crop = IntVar(window, value=0)
-            occult_file_info["crop"] = occult_crop
-            ttk.Checkbutton(window, text="Crop", variable=occult_crop).grid(
-                sticky="e", row=current_row, column=0)
-            occult_color = IntVar(window, value=0)
-            occult_file_info["color"] = occult_color
-            ttk.Radiobutton(window, text="Use File Color", variable=occult_color, value=0).grid(
-                row=current_row, column=1)
-            ttk.Radiobutton(window, text="Overwrite Color", variable=occult_color, value=1).grid(
-                row=current_row, column=2)
-            occult_color_entry = ttk.Entry(window, width=7)
-            occult_file_info["color_info"] = occult_color_entry
-            occult_color_entry.insert(0, f"{occult_color_list[index]}")
-            occult_color_entry.grid(sticky="w", row=current_row, column=3)
-            current_row += 1
-
-            occult = IntVar(window, value=1)
-            occult_file_info["occult"] = occult
-            ttk.Checkbutton(window, text="Occult", variable=occult).grid(
-                sticky="e", row=current_row, column=0)
-            occult_keep_lines = IntVar(window, value=0)
-            occult_file_info["keep"] = occult_keep_lines
-            ttk.Checkbutton(window, text="Keep occulted lines", variable=occult_keep_lines).grid(
-                sticky="w", row=current_row, column=1)
-            occult_ignore = IntVar(window, value=1)
-            occult_file_info["ignore"] = occult_ignore
-            ttk.Checkbutton(window, text="Ignores layers", variable=occult_ignore).grid(
-                sticky="w", row=current_row, column=2)
-            occult_across = IntVar(window, value=0)
-            occult_file_info["across"] = occult_across
-            ttk.Checkbutton(window, text="Occult across layers,\nnot within",
-                            variable=occult_across).grid(sticky="w", row=current_row, column=3)
-            current_row += 1
-
-            ttk.Separator(window, orient='horizontal').grid(
-                sticky="we", row=current_row, column=0, columnspan=4, pady=10)
-            current_row += 1
-
-            occult_info_list.append(occult_file_info)
+    occult_info_list.append(occult_file_info)
 
     ttk.Button(window, text="Show Output", command=show_vpypeline).grid(
         pady=(0, 10), row=current_row, column=0)
