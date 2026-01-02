2ccb51051b479171c993539bc6e7a993d400f5bd
lewi0622 <eric.lewiston.el@gmail.com>
2026-01-02 16:56:18 -0600

    added file for placing default values that apps use. settings parse the defaults and pyproject files. making those values available

--- settings.py
+++ settings.py
@@ -1,6 +1,7 @@
 """Functions for stored graphical settings"""
 
 import sys
+import tomllib
 from typing import Any
 
 try:
@@ -26,3 +27,26 @@ def set_theme(root: Any) -> None:
     """Applies theme to ttk"""
     if THEME != "":
         sv_ttk.set_theme(THEME, root)
+
+
+with open("pyproject.toml", "rb") as f:
+    pyproject_data = tomllib.load(f)
+    VERSION = pyproject_data.get("project", {}).get("version", "")
+
+GLOBAL_DEFAULTS: dict[str, Any] = {}
+COMPOSE_DEFAULTS: dict[str, Any] = {}
+DECOMPOSE_DEFAULTS: dict[str, Any] = {}
+MAIN_APPLICATION_DEFAULTS: dict[str, Any] = {}
+OCCULT_DEFAULTS: dict[str, Any] = {}
+PAINT_DEFAULTS: dict[str, Any] = {}
+PROCESS_DEFAULTS: dict[str, Any] = {}
+
+with open("defaults.toml", "rb") as f:
+    settings_data = tomllib.load(f)
+    GLOBAL_DEFAULTS = settings_data.get("global", {})
+    COMPOSE_DEFAULTS = settings_data.get("compose", {})
+    DECOMPOSE_DEFAULTS = settings_data.get("decompose", {})
+    MAIN_APPLICATION_DEFAULTS = settings_data.get("main_application", {})
+    OCCULT_DEFAULTS = settings_data.get("occult", {})
+    PAINT_DEFAULTS = settings_data.get("paint", {})
+    PROCESS_DEFAULTS = settings_data.get("process", {})
